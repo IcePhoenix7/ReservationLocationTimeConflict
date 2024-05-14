f7aeec309bf6981df6d1aeed4445080e89d5b54a
Mohammed Moallem <mohamadfm5@gmail.com>
2024-05-14 19:44:17 +0300

    this system depend on devising the time into intervals

--- src/Location.java
+++ src/Location.java
@@ -50,7 +50,7 @@ public class Location {
 
     public String getOpenTime() {
         return openTime;
-    }
+    }//
 
     public void setOpenTime(String openTime) {
         this.openTime = openTime;
